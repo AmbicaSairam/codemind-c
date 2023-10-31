e762cb8313909d23c69a520b01090ddab0d946b9
AmbicaSairam <147791763+AmbicaSairam@users.noreply.github.com>
2023-10-31 21:04:33 +0530

    #include<stdio.h> int main() {     int m,n=0;     scanf("%d",&m);     if(m==0)     {         printf("1");     }     else     {         printf("%d",m*3);     }
    }

---
+++ Ramanujan_and_0.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int m,n=0;
+    scanf("%d",&m);
+    if(m==0)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("%d",m*3);   
+    }
+    
+}
