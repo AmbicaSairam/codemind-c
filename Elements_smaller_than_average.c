3354c586ec1b5dc6bf4cd196419f18a24c33b9cf
AmbicaSairam <147791763+AmbicaSairam@users.noreply.github.com>
2023-12-20 05:48:59 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for(int i=0;i<n;i++)     {         sum=sum+arr[i];     }     int c=0;      for(int i=0;i<n;i++)     {         if(arr[i]<=(sum/n))         {             c++;         }     }     printf("%d",c);
    }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    int c=0;
+     for(int i=0;i<n;i++)
+    {
+        if(arr[i]<=(sum/n))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
